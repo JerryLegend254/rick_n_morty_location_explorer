9d5088750368e1f1bad76f45ff8f0fbaac38b809
JerryLegend254 <jerryokuto713@gmail.com>
2024-02-17 13:21:23 +0300

    added apollo wrappers for ssr and csr

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,10 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const createApolloClient = () => {
+// const createApolloClient = () => {
+//   return new ApolloClient({
+//     uri: "https://rickandmortyapi.com/graphql",
+//     cache: new InMemoryCache(),
+//   });
+// };
+
+// export default createApolloClient;
+
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+
+export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
-    uri: "https://rickandmortyapi.com/graphql",
     cache: new InMemoryCache(),
+    link: new HttpLink({
+      // this needs to be an absolute url, as relative urls cannot be used in SSR
+      uri: "https://rickandmortyapi.com/graphql",
+      // you can disable result caching here if you want to
+      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+      // fetchOptions: { cache: "no-store" },
+    }),
   });
-};
-
-export default createApolloClient;
+});
